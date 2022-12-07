7cb94242a171317985fa23f19cfe94acd7bf2eb7
pchae <20016424+pchae@users.noreply.github.com>
2022-12-07 14:51:45 -0500

    update paragraph text colour

--- pages/about.vue
+++ pages/about.vue
@@ -20,12 +20,12 @@
             <div>
                 <div class="container pb-8">
                     <p class="text-4xl text-center text-white pb-8">Keeping it 100</p>
-                    <p class="text-md text-white">We believe that our work and ethics
+                    <p class="text-md text-gray">We believe that our work and ethics
                     define who we are. We're Human-centered Designers and Developers behind some of North America's
                     biggest mobile apps and digital brands, looking to make a net-positive impact in the world.</p>
                 </div>
                 <div class="container">
-                <p class="text-md text-white">Our approach to building software
+                <p class="text-md text-gray">Our approach to building software
                     prioritizes accessibility, user-centric design, and inclusion. We research and generate ideas, then
                     build, test, and iterate on them using real-world analytics and actionable KPIs. The emphasis on
                     data-driven design, diversity, user empathy, and sustainability aren't buzzwords, they're why we
