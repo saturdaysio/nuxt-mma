5e4527cb64e35829c7931a0f0ce098188867a7a5
pchae <20016424+pchae@users.noreply.github.com>
2022-12-07 18:52:56 -0500

    update layout

--- pages/index.vue
+++ pages/index.vue
@@ -9,18 +9,26 @@
 
         <Hero />
 
+
         <section class="container mx-auto py-16">
             <div class="">
                 <p class="text-4xl text-center text-white">
-                    We're a Data-driven digital studio that focuses on the Design Development and Strategy of digital
-                    products.
+                    Event countdown:
                 </p>
+                <p class="text-xl text-center text-gray">5 Days 16 Hours 22 Minutes</p>
             </div>
         </section>
 
-        <Client />
+            <Fighter />
 
-        <Result />
+        <section class="container mx-auto py-16 px-4">
+            <div class="">
+                <p class="text-4xl text-white">
+                    We're a Data-driven digital studio that focuses on the Design Development and Strategy of digital
+                    products. Second line of text to extend words on screen.
+                </p>
+            </div>
+        </section>
 
         <Contact />
 
