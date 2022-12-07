c80832c2ad23bdcf3bdfed54f3ba192e98208105
pchae <20016424+pchae@users.noreply.github.com>
2022-12-07 18:51:43 -0500

    update layout

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,9 +1,15 @@
 <template>
-  <section class="container mx-auto py-16">
-    <div class="align-middle">
-      <p class="text-6xl text-center align-middle text-white">
-        Saturdays is a Canadian digital studio based in Toronto
-      </p>
+  <section class="container mx-auto h-96 py-16">
+    <div class="flex flex-col">
+      <div class="w-full h-full align-middle">
+        <p class="text-6xl text-center text-white">
+          UFC 282
+        </p>
+        <p class="text-2xl text-center text-gray">
+          Blachowicz vs. Ankalaev
+        </p>
+        <p class="text-md text-center text-gray">December 10, 2022 • T-Mobile Arena, Las Vegas, Nevada</p>
+      </div>
     </div>
   </section>
 </template>
