f8010ca5f7c984ecade54a9d85696e5919198cd3
pchae <20016424+pchae@users.noreply.github.com>
2022-12-07 12:20:15 -0500

    remove module styling

--- pages/about.vue
+++ pages/about.vue
@@ -8,24 +8,24 @@
 
         <Nav />
 
-        <section :class="[$style.container, $style.mxAuto, $style.py16]">
-            <div :class="$style.content">
-                <p :class="[$style.text6xl, $style.textCenter, $style.textWhite]">
+        <section class="container mx-auto py-16">
+            <div class="content">
+                <p class="text-6xl text-center text-white">
                     We bridge the gap between design and technology
                 </p>
             </div>
         </section>
 
-        <section id="ethics" :class="[$style.container, $style.mxAuto, $style.py16]">
+        <section id="ethics" class="container mx-auto py-16">
             <div>
-                <div :class="[$style.container, $style.pb4]">
-                    <p :class="[$style.text4xl, $style.textCenter, $style.textWhite, $style.pb8]">Keeping it 100</p>
-                    <p :class="[$style.textMd, $style.textWhite]">We believe that our work and ethics
+                <div class="container pb-8">
+                    <p class="text-4xl text-center text-white pb-8">Keeping it 100</p>
+                    <p class="text-md text-white">We believe that our work and ethics
                     define who we are. We're Human-centered Designers and Developers behind some of North America's
                     biggest mobile apps and digital brands, looking to make a net-positive impact in the world.</p>
                 </div>
-                <div :class="$style.container">
-                <p :class="[$style.textMd, $style.textWhite]">Our approach to building software
+                <div class="container">
+                <p class="text-md text-white">Our approach to building software
                     prioritizes accessibility, user-centric design, and inclusion. We research and generate ideas, then
                     build, test, and iterate on them using real-world analytics and actionable KPIs. The emphasis on
                     data-driven design, diversity, user empathy, and sustainability aren't buzzwords, they're why we
--- pages/privacy.vue
+++ pages/privacy.vue
@@ -7,14 +7,14 @@
 
     <Nav />
 
-    <section id="legal" :class="[$style.container, $style.mxAuto]">
-      <div :class="$style.py16">
+    <section id="legal" class="container mx-auto">
+      <div class="py16">
         <p class="text-6xl text-white">Privacy Policy</p>
-        <p :class="[$style.textWhite]">Last updated: Jan 25, 2022</p>
+        <p class="text-white">Last updated: Jan 25, 2022</p>
       </div>
 
-      <div id="definition" :class="[$style.container, $style.pb8]">
-        <p :class="$style.textWhite">
+      <div id="definition" class="container pb-8">
+        <p class="text-white">
           Saturdays.io, Inc. (“our,” “we,” or “us”), takes your (“you,” “your,” or the “User”) privacy seriously. We may
           collect, use, and share some of your personal information. This Privacy Policy is a guide to explain how we do
           that, and your rights as a User. This Privacy Policy forms part of our Terms of Service available at <NuxtLink
@@ -22,22 +22,22 @@
         </p>
       </div>
 
-      <div id="use-of-services" :class="[$style.container, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite, $style.pb4]">
+      <div id="use-of-services" class="container pb-8">
+        <p class="text-4xl text-white pb4">
           1. Policy Scope
         </p>
-        <p :class="[$style.textWhite, $style.pb4]">
+        <p class="text-white pb-4">
           This Privacy Policy covers our treatment of your personally identifiable information and any other information
           you may provide us when using our applications, websites, container, features, products or other services
           (referred to herein as the “Services”).
         </p>
       </div>
 
-      <div id="info-collected" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="info-collected" class="container pb-8">
+        <p class="text-4xl text-white">
           2. Information We Collect
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           We focus on using your personal information to provide and improve our Services, respond to your requests,
           service user accounts connected to our Services, provide you with services or information you may request,
           communicate with you, and inform you about new products and services that may be of interest and to improve
@@ -46,67 +46,67 @@
 
         <ol type="1">
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Information You Provide Us: Name, mailing address, phone number, email address, contact preferences,
               device identifiers, IP address, location information, credit card information, and profile information
               where the contact is via social media.
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Information We Collect from Users of our Services: Name, mailing address, Phone number, Email address,
               Contact preferences, Device identifiers, IP address, and location information.
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Information We Collect from Third Parties: We don't collect information from third parties.
             </p>
           </li>
         </ol>
       </div>
 
-      <div id="how-we-use-info" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="how-we-use-info" class="container pb-8">
+        <p class="text-4xl text-white">
           3. How We Use the Information We Collect
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           We may use the information that we collect for any of the following purposes:
         </p>
 
         <ol type="1">
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Improve Service functionality and efficiency
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Provide, maintain and improve our Services
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Provide assistance to users
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Personalize our Services
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Measure and better understand the performance of our Services
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Communicate with Users
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Protect Users, the community, us and other legal reasons such as complying with applicable laws, assisting
               with law enforcement agencies if legally mandated, to find instances of abuse or misuse of our services or
               illegal activity, maintain security and safety when using our Services
@@ -114,80 +114,80 @@
           </li>
         </ol>
 
-        <p :class="$style.textWhite">
+        <p class="text-white">
           In the event that we determine we need to use your personal information for a reason not included in this
           Privacy Policy, we will ask for your consent first.
         </p>
       </div>
 
-      <div id="share-info" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="share-info" class="container pb-8">
+        <p class="text-4xl text-white">
           4. Sharing Information
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           We do not sell your personal information. To better provide Services, we may share your personal information
           with third parties (like social networks), and our service providers.
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           We may share your information with third parties in other cases such as:
         </p>
 
         <ol type="1">
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               If a government body requests information and we deemed it to be necessary to disclose such information in
               order to comply with applicable laws and legal processes;
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               To cooperate with law enforcement or other government authorities as it relates to any investigations or
               other inquiries to the extent, we deem it necessary to protect either our users, the public, or us.
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               To defend or pursue legal claims as required or permitted by law.
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Enforcing our Terms of Service.
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               Responding to any legal process including court orders and search warrants.
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               To detect or protect against fraud, security, technical or legal issues associated with the use of our
               Services.
             </p>
           </li>
           <li>
-            <p :class="$style.textWhite">
+            <p class="text-white">
               At any time that you consent to such sharing.
             </p>
           </li>
         </ol>
       </div>
 
-      <div id="control-info" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="control-info" class="container pb-8">
+        <p class="text-4xl text-white">
           5. Controlling Your Information
         </p>
-        <p :class="[$style.textWhite, $style.pb4]">
+        <p class="text-white pb-4">
           You have control over the information that you share with us subject to certain limitations.
         </p>
-        <p :class="[$style.textWhite, $style.pb4]">
+        <p class="text-white pb-4">
           Our Services have been built in a way to minimize the amount of personal information that is stored within
           them. Nonetheless, if you want to review, update, change or delete the information that you have shared with
           us, including deletion of your account, you may send a request to do so to <NuxtLink
             to="mailto:support@saturdays.io">support [at] saturdays.io</NuxtLink>.
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           Even in the event that we delete your personal information, we still may retain some information for
           legitimate business purposes such as maintenance of security of the Services or to comply with applicable laws
           or regulations, depending on the circumstances of such deletion. You understand and agree that we may have
@@ -197,42 +197,42 @@
         </p>
       </div>
 
-      <div id="do-not-track" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="do-not-track" class="container pb-8">
+        <p class="text-4xl text-white">
           6. Do Not Track
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           We do not track our Users over time and across third party websites and therefore our Services do not respond
           to Do Not Track signals.
         </p>
       </div>
 
-      <div id="gdpr" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="gdpr" class="container pb-8">
+        <p class="text-4xl text-white">
           7. GDPR
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           We do not collect “personal data” about, or monitor behavior of, “data subjects” as those terms are defined in
           GDPR Art. 4(1). We do not “target” individuals in the EEU.
         </p>
       </div>
 
-      <div id="age-restrictions" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="age-restrictions" class="container pb-8">
+        <p class="text-4xl text-white">
           8. Age Restrictions
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           We do not knowingly collect or solicit the personal information of anyone under the age of 13, even with a
           parent or guardian's consent. Our services are not intended for children and in the event that we determine
           that a person under 13 has provided personal information, we will delete that information immediately.
         </p>
       </div>
 
-      <div id="do-not-track" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="do-not-track" class="container pb-8">
+        <p class="text-4xl text-white">
           9. Changes to this Privacy Policy
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           We may update this Privacy Policy from time to time as necessary based on changes to our company, our
           Services, or to applicable laws. In the event that we do update this Privacy Policy, such changes will be made
           online. You will be notified of any material changes through the Services or e-mail. Your use of the Services
@@ -240,11 +240,11 @@
         </p>
       </div>
 
-      <div id="assignment" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="assignment" class="container pb-8">
+        <p class="text-4xl text-white">
           10. Assignment, Change of Control and Transfer
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           All of our rights and obligations pursuant to this Privacy Policy are freely assignable by us to any of our
           affiliates, in connection with a merger, acquisition, restructuring, or sale of assets, or by operation of law
           or otherwise, and we may transfer your information to any of our affiliates, successor entities, or a new
@@ -252,11 +252,11 @@
         </p>
       </div>
 
-      <div id="contact-us" :class="[$style.container, $style.isNormal, $style.pb8]">
-        <p :class="[$style.text4xl, $style.textWhite]">
+      <div id="contact-us" class="container pb-8">
+        <p class="text-4xl text-white">
           11. Contact Us
         </p>
-        <p :class="$style.textWhite">
+        <p class="text-white">
           For any questions or concerns that you may have regarding this Privacy Policy, please contact us at <NuxtLink
             to="mailto:support@saturdays.io">support [at] saturdays.io</NuxtLink>.
         </p>
